60eaa843e836d6f7f86a0340a6ba75cf546855d8
Alex-Kamenev <akamenev64@gmail.com>
2020-03-12 22:34:31 -0400

    Add files via upload
    will need to spin up a localhost server
    in the terminal type node calculator.js
    Node.js is required
    Go to
    http://localhost:3000/
    http://localhost:3000/bmiCalculator

---
+++ Calculator/calculator.js
@@ -0,0 +1,39 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+  res.send("The result of the calculation is: " + result);
+});
+
+app.get("/bmiCalculator", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function(req, res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var bmiResult = weight/Math.pow(height, 2);
+
+  res.send("<h1>BMI Calculator</h1><br>Your BMI is: " + bmiResult);
+
+});
+
+app.listen(3000, function() {
+  console.log("Server akamenev is started on 3000 port.");
+});
